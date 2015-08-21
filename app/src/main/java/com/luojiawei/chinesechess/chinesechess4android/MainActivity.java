43e99abec2e21340300d59ff2e2f3ff4b6384acc
L1 <Getnway@gmail.com>
2015-08-21 12:15:12 +0800

    修改icon图标，完成初始界面

--- app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/MainActivity.java
+++ app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/MainActivity.java
@@ -1,17 +1,63 @@
 package com.luojiawei.chinesechess.chinesechess4android;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button btnLowRank;
+    private Button btnMiddleRank;
+    private Button btnHighRank;
+    private Button btnBluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置无标题，全屏显示
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         setContentView(R.layout.activity_main);
+
+        //找按钮控件，并设置监听
+        btnLowRank = (Button) findViewById(R.id.btn_low_rank);
+        btnLowRank.setOnClickListener(this);
+        btnMiddleRank = (Button) findViewById(R.id.btn_middle_rank);
+        btnMiddleRank.setOnClickListener(this);
+        btnHighRank = (Button) findViewById(R.id.btn_high_rank);
+        btnHighRank.setOnClickListener(this);
+        btnBluetooth = (Button) findViewById(R.id.btn_bluetooth);
+        btnBluetooth.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_low_rank:
+                LogUtil.i(getLocalClassName(), "btn_low_rank");
+                break;
+            case R.id.btn_middle_rank:
+                LogUtil.i(getLocalClassName(), "btn_middle_rank");
+                break;
+            case R.id.btn_high_rank:
+                LogUtil.i(getLocalClassName(), "btn_high_rank");
+                break;
+            case R.id.btn_bluetooth:
+                LogUtil.i(getLocalClassName(), "btn_bluetooth");
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
