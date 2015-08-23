c16f103fd6720d2a3f6313d0acd0b76200630b0e
L1 <Getnway@gmail.com>
2015-08-23 16:19:57 +0800

    增加棋盘工具类，修改棋盘，并绘制棋子

---
+++ app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/ChessboardUtil.java
@@ -0,0 +1,135 @@
+package com.luojiawei.chinesechess.chinesechess4android;
+
+/**
+ * Created by L1 on 15-08-23.
+ */
+public class ChessboardUtil {
+    public static int sdPlayer;                   // 轮到谁走，0=红方，1=黑方
+    public static int[] currentMap = new int[256];          // 棋盘上的棋子
+
+    // 棋盘范围
+    public final static int BOARD_TOP = 3;
+    public final static int BOARD_BOTTOM = 12;
+    public final static int BOARD_LEFT = 3;
+    public final static int BOARD_RIGHT = 11;
+
+    // 棋子编号
+    public final static int PIECE_KING = 0;
+    public final static int PIECE_ADVISOR = 1;
+    public final static int PIECE_BISHOP = 2;
+    public final static int PIECE_KNIGHT = 3;
+    public final static int PIECE_ROOK = 4;
+    public final static int PIECE_CANNON = 5;
+    public final static int PIECE_PAWN = 6;
+
+
+    public static void startup() {            // 初始化棋盘
+        sdPlayer = 0;
+        for (int i = 0; i < 256; i++) {
+            currentMap[i] = startupMap[i];
+        }
+    }
+
+    public static void changeSide() {         // 交换走子方
+        sdPlayer = 1 - sdPlayer;
+    }
+
+    public static void addPiece(int sq, int pc) { // 在棋盘上放一枚棋子
+        currentMap[sq] = pc;
+    }
+
+    public static void delPiece(int sq) {         // 从棋盘上拿走一枚棋子
+        currentMap[sq] = 0;
+    }
+
+    public static void movePiece(int mv) {         // 搬一步棋的棋子
+        int sqSrc, sqDst, pc;
+        sqSrc = getMoveSrc(mv);
+        sqDst = getMoveDst(mv);
+        delPiece(sqDst);
+        pc = currentMap[sqSrc];
+        delPiece(sqSrc);
+        addPiece(sqDst, pc);
+    }
+
+    public static void makeMove(int mv) {         // 走一步棋
+        movePiece(mv);
+        changeSide();
+    }
+
+    //起始棋局
+    public static final short[] startupMap = {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0,20,19,18,17,16,17,18,19,20, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0,21, 0, 0, 0, 0, 0,21, 0, 0, 0, 0, 0,
+            0, 0, 0,22, 0,22, 0,22, 0,22, 0,22, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0,14, 0,14, 0,14, 0,14, 0,14, 0, 0, 0, 0,
+            0, 0, 0, 0,13, 0, 0, 0, 0, 0,13, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0,12,11,10, 9, 8, 9,10,11,12, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    };
+
+    // 获得格子的纵坐标
+    public static int getCoordY(int sq) {
+        return sq >> 4;
+    }
+
+    // 获得格子的横坐标
+    public static int getCoordX(int sq) {
+        return sq & 15;
+    }
+
+    // 根据横纵坐标获得格子
+    public static int getCoordPoint(int x, int y) {
+        return x + (y << 4);
+    }
+
+    // 中心翻转棋盘（180°）
+    public static int centreFlip(int sq) {
+        return 254 - sq;
+    }
+
+    // 横坐标水平镜像
+    public static int horizontalFlip(int x) {
+        return 14 - x;
+    }
+
+    // 纵坐标垂直镜像
+    public static int verticlFlip(int y) {
+        return 15 - y;
+    }
+
+    // 获得红黑标记(红子是8，黑子是16)
+    public static int getSideTag(int sd) {
+        return 8 + (sd << 3);
+    }
+
+    // 获得对方红黑标记
+    public static int getOppositeSideTag(int sd) {
+        return 16 - (sd << 3);
+    }
+
+    // 获得走法的起点
+    public static int getMoveSrc(int mv) {
+        return mv & 255;
+    }
+
+    // 获得走法的终点
+    public static int getMoveDst(int mv) {
+        return mv >> 8;
+    }
+
+    // 根据起点和终点获得走法
+    public static int getMove(int sqSrc, int sqDst) {
+        return sqSrc + sqDst << 8;
+    }
+
+}
--- app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/GameView.java
+++ app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/GameView.java
@@ -14,13 +14,15 @@ import android.widget.ImageView;
  * 游戏界面与控制
  */
 public class GameView extends ImageView {
+    String Tag = "GameView";
     int mScreenW, mScreenH; //屏幕宽高
     int mBoardMargin;   //棋盘上（下）边缘外边距
     int mChessSize;  //棋子长宽
     float clickX, clickY;   //点击坐标
     int row = -1, column = -1;
-    Bitmap mBmChessboard;
-    Bitmap mBmSelectFrom;
+    Bitmap mBmChessboard;   //棋盘
+    Bitmap[] mBmAllChess = new Bitmap[14]; //棋子
+    Bitmap mBmSelectFrom, mBmSelectTo;   //选择框
     Rect mBoardDst; //棋盘目标位置
 
     public GameView(Context context) {
@@ -30,8 +32,8 @@ public class GameView extends ImageView {
         mScreenW = dm.widthPixels;
         mScreenH = dm.heightPixels;
 
-        mBmSelectFrom = BitmapFactory.decodeResource(getResources(), R.drawable.sel);
-        mBmChessboard = BitmapFactory.decodeResource(getResources(), R.drawable.board);
+        loadResoure();
+        ChessboardUtil.startup();
 //        float ratioHW = (float) mBmChessboard.getHeight() / mBmChessboard.getWidth();   //根据棋盘资源算出高宽比
 //        mBoardMargin = (int)((mScreenH-mScreenW * ratioHW)/2);  //再根据当前屏幕宽度算出对应高度，进而算出棋盘上（下）边缘外边距
         mChessSize = mScreenW / 9;    //横向9个棋位
@@ -42,14 +44,132 @@ public class GameView extends ImageView {
         setBackgroundResource(R.drawable.bg2);
 
         LogUtil.i("GameView", "Screen:" + String.valueOf(mScreenH) + " X " + String.valueOf(mScreenW));
-        LogUtil.i("GameView", "ChessSize:"+String.valueOf(mChessSize));
+        LogUtil.i("GameView", "ChessSize:" + String.valueOf(mChessSize));
+    }
+
+    /**
+     * 载入资源
+     */
+    private void loadResoure() {
+        mBmChessboard = BitmapFactory.decodeResource(getResources(), R.drawable.board2);
+        mBmSelectFrom = BitmapFactory.decodeResource(getResources(), R.drawable.sel);
+        mBmSelectTo = BitmapFactory.decodeResource(getResources(), R.drawable.sel);
+        Bitmap tmpAllChess = BitmapFactory.decodeResource(getResources(), R.drawable.qz);
+        int chessSize = tmpAllChess.getHeight() / 3;  //棋子图片为3行14列
+        for (int i = 0; i < 14; ++i) {
+            mBmAllChess[i] = Bitmap.createBitmap(tmpAllChess, i * chessSize, 0, chessSize, chessSize);
+        }
+    }
+
+    /**
+     * 根据棋盘横坐标计算屏幕实际横坐标
+     *
+     * @param x 棋盘横坐标
+     * @return 屏幕实际横坐标
+     */
+    private int board2ScreenX(int x) {
+        return x * mChessSize;
+    }
+
+    /**
+     * 根据棋盘纵坐标计算屏幕实际纵坐标
+     *
+     * @param y 棋盘纵坐标
+     * @return 屏幕实际纵坐标
+     */
+    private int board2ScreenY(int y) {
+        return y * mChessSize + mBoardMargin;
+    }
+
+    /**
+     * 根据屏幕点击横坐标计算对应棋盘横坐标
+     *
+     * @param clickX 屏幕点击横坐标
+     * @return 棋盘横坐标
+     */
+    private int screen2BoardX(float clickX) {
+        return (int) (clickX / mChessSize);
+    }
+
+    /**
+     * 根据屏幕点击纵坐标计算对应棋盘纵坐标
+     *
+     * @param clickY 屏幕点击纵坐标
+     * @return 棋盘纵坐标
+     */
+    private int screen2BoardY(float clickY) {
+        return (int) ((clickY - mBoardMargin) / mChessSize);
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-        canvas.drawBitmap(mBmChessboard, null, mBoardDst, null);    //绘制棋盘
+        //绘制棋盘
+        canvas.drawBitmap(mBmChessboard, null, mBoardDst, null);
+
+        //绘制棋子
+        for (int i = 0; i < 256; ++i) {
+            if (ChessboardUtil.currentMap[i] != 0) {
+                drawPiece(canvas, i);
+            }
+        }
+
+        //绘制选择框
         if (row >= 0 && column >= 0) {
-            canvas.drawBitmap(mBmSelectFrom, null, new Rect(column * mChessSize, mBoardMargin + row * mChessSize, column * mChessSize+mChessSize, mBoardMargin + row * mChessSize+mChessSize), null);
+            canvas.drawBitmap(mBmSelectFrom, null, new Rect(board2ScreenX(column), board2ScreenY(row), board2ScreenX(column) + mChessSize, board2ScreenY(row) + mChessSize), null);
+        }
+    }
+
+    /**
+     * 绘制棋子
+     * @param canvas Canvas对象
+     * @param position 棋子在棋盘的位置
+     */
+    private void drawPiece(Canvas canvas, int position) {
+        int screenX = board2ScreenX(ChessboardUtil.getCoordX(position) - ChessboardUtil.BOARD_LEFT);
+        int screenY = board2ScreenY(ChessboardUtil.getCoordY(position) - ChessboardUtil.BOARD_TOP);
+        switch (ChessboardUtil.currentMap[position]) {
+            case 16:    //帅
+                canvas.drawBitmap(mBmAllChess[0], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 17:    //仕
+                canvas.drawBitmap(mBmAllChess[1], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 18:    //相
+                canvas.drawBitmap(mBmAllChess[2], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 19:    //马
+                canvas.drawBitmap(mBmAllChess[3], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 20:    //车
+                canvas.drawBitmap(mBmAllChess[4], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 21:    //砲
+                canvas.drawBitmap(mBmAllChess[5], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 22:    //兵
+                canvas.drawBitmap(mBmAllChess[6], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 8:    //将
+                canvas.drawBitmap(mBmAllChess[7], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 9:    //士
+                canvas.drawBitmap(mBmAllChess[8], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 10:    //象
+                canvas.drawBitmap(mBmAllChess[9], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 11:    //马
+                canvas.drawBitmap(mBmAllChess[10], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 12:    //车
+                canvas.drawBitmap(mBmAllChess[11], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 13:    //炮
+                canvas.drawBitmap(mBmAllChess[12], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
+            case 14:    //卒
+                canvas.drawBitmap(mBmAllChess[13], null, new Rect(screenX, screenY, screenX + mChessSize, screenY + mChessSize), null);
+                break;
         }
     }
 
@@ -59,8 +179,8 @@ public class GameView extends ImageView {
         clickY = event.getY();
         //点击在棋盘内
         if (clickY >= mBoardMargin && clickY <= mScreenH - mBoardMargin) {
-            column = (int) (clickX / mChessSize);    //根据横坐标计算点击在第几列
-            row = (int) ((clickY - mBoardMargin) / mChessSize);   //根据纵坐标计算点击在第几行
+            column = screen2BoardX(clickX);    //根据横坐标计算点击在第几列
+            row = screen2BoardY(clickY);   //根据纵坐标计算点击在第几行
             LogUtil.i("GameView", "in :" + String.valueOf(row) + "--" + String.valueOf(column));
         }
         LogUtil.i("GameView", "coord: Y=" + String.valueOf(clickY - mBoardMargin) + "--X=" + String.valueOf(clickX) + "--Size=" + String.valueOf(mChessSize));
