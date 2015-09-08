fa27d5d6f6fa522d0b1aaf77b31b1d3ffe6c2a9d
L1 <Getnway@gmail.com>
2015-09-08 23:50:56 +0800

    成员变非静态（静态将导致同一变量）

--- app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/HashItem.java
+++ app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/HashItem.java
@@ -4,12 +4,12 @@ package com.luojiawei.chinesechess.chinesechess4android;
  * Created by L1 on 15-09-04.
  */
 public class HashItem {
-    static int ucDepth, ucFlag;
-    static int mv, reserved;
-    static int vl;
-    static long lock0, lock1;
+    int ucDepth, ucFlag;
+    int mv, reserved;
+    int vl;
+    long lock0, lock1;
 
-    public static void init() {
+    public void init() {
         ucDepth = ucFlag = 0;
         mv = reserved = vl =0;
         lock0 = lock1 = 0;
