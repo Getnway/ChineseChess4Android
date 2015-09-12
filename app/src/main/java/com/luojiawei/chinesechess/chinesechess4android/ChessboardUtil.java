9e83fbbd6f36fff194945579cb754ad6bcd52b1a
L1 <Getnway@gmail.com>
2015-09-12 23:40:29 +0800

    修改棋盘打印函数

--- app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/ChessboardUtil.java
+++ app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/ChessboardUtil.java
@@ -384,7 +384,7 @@ public class ChessboardUtil {
     }
 
     // ‰≥ˆ∆Â≈Ã
-    public static void printBoard(int nDistance, String tag){
+    public static String printBoard(int nDistance, int[] board){
         String[] str = {"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"};
         StringBuffer sb = new StringBuffer();
         sb.append("nDistance: ");
@@ -403,9 +403,9 @@ public class ChessboardUtil {
             }else{
                 sb.append("\t");
             }
-            sb.append(currentMap[i]);
+            sb.append(board[i]);
         }
-        LogUtil.i(tag, sb.toString());
+        return sb.toString();
     }
 
 }
