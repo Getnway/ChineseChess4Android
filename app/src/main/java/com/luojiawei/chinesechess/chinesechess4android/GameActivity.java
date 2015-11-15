03eef1251719d35c4132f11bf76fc9c963e0eef0
L1 <Getnway@gmail.com>
2015-11-15 13:16:23 +0800

    增加蓝牙，未完善

--- app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/GameActivity.java
+++ app/src/main/java/com/luojiawei/chinesechess/chinesechess4android/GameActivity.java
@@ -1,9 +1,15 @@
 package com.luojiawei.chinesechess.chinesechess4android;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,24 +17,39 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class GameActivity extends Activity implements View.OnClickListener {
-
-    final static int BLUETOOTH = 0;
-    final static int LOW_RANK = 1;
-    final static int MIDDLE_RANK = 2;
-    final static int HIGH_RANK = 3;
-
     String TAG = "GameActivity";
-    TextView mTxtOppositeName;
-    Button mBtnNewGame;
-    Button mBtnFlipBoard;
-    Button mBtnUndo;
-    GameView gameView;
-    final static int REQUEST_SETTING = 100;
-    private Intent intentSetting;
-    int level = 1;
+
+    //模式等级
+    public static final int BLUETOOTH = 0;
+    public static final int LOW_RANK = 1;
+    public static final int MIDDLE_RANK = 2;
+    public static final int HIGH_RANK = 3;
+
+    //Request Code
+    private static final int REQUEST_SETTING = 100;
+    private static final int REQUEST_CONNECT_DEVICE = 101;
+    private static final int REQUEST_ENABLE_BLUETOOTH = 102;
+
+    //View
+    private TextView mTxtOppositeName;
+    private Button mBtnNewGame;
+    private Button mBtnFlipBoard;
+    private Button mBtnUndo;
+    private GameView gameView;      //棋盘View
+
+    //与蓝牙相关的变量
+    private String mConnectedDeviceName = null;         //已连接设备
+    private StringBuffer mOutStringBuffer;              //接收到的数据
+    private BluetoothAdapter mBluetoothAdapter = null;  //蓝牙适配器
+    private BluetoothService mBluetoothService = null;  //蓝牙服务
+
+
+    private Intent intentSetting;   //设置意图
+    private int level;              //模式等级
 
     public static void actionStart(Context context, int level){
         Intent intent = new Intent(context, GameActivity.class);
@@ -47,6 +68,7 @@ public class GameActivity extends Activity implements View.OnClickListener {
         );
         setContentView(R.layout.activity_game);
 
+        //查找View
         mTxtOppositeName = (TextView)findViewById(R.id.txt_opposite_name);
         mTxtOppositeName.setVisibility(View.INVISIBLE);
         mBtnNewGame = (Button)findViewById(R.id.btn_new_game);
@@ -55,11 +77,12 @@ public class GameActivity extends Activity implements View.OnClickListener {
         mBtnFlipBoard.setOnClickListener(this);
         mBtnUndo = (Button)findViewById(R.id.btn_undo);
         mBtnUndo.setOnClickListener(this);
-
         gameView = (GameView)findViewById(R.id.game_view);
 
+        //设置意图
         intentSetting = new Intent(this, Setting.class);
 
+        //获取模式等级，初始化游戏参数
         Intent intent = getIntent();
         level = intent.getIntExtra("level", LOW_RANK);
         LogUtil.d(TAG, "" + level);
@@ -67,18 +90,21 @@ public class GameActivity extends Activity implements View.OnClickListener {
             case BLUETOOTH: //蓝牙对战
                 mTxtOppositeName.setVisibility(View.VISIBLE);
                 gameView.AI = false;
+                if(!isSupportBluetooth()){
+                    finish();
+                }
                 break;
-            case LOW_RANK: //初级
-                gameView.AI = true;
-                Engine.MIN_LEVEL = 1;
-                Engine.LIMIT_TIME = 100;
+            case LOW_RANK:      //初级
+                gameView.AI = true;     //是否人机
+                Engine.MIN_LEVEL = 1;   //最低搜索层次
+                Engine.LIMIT_TIME = 30; //限制时间（ms）
                 break;
-            case MIDDLE_RANK: //中级
+            case MIDDLE_RANK:   //中级
                 gameView.AI = true;
                 Engine.MIN_LEVEL = 3;
-                Engine.LIMIT_TIME = 1000;
+                Engine.LIMIT_TIME = 800;
                 break;
-            case HIGH_RANK: //高级
+            case HIGH_RANK:     //高级
                 gameView.AI = true;
                 Engine.MIN_LEVEL = 3;
                 Engine.LIMIT_TIME = 5000;
@@ -88,6 +114,66 @@ public class GameActivity extends Activity implements View.OnClickListener {
                 Engine.MIN_LEVEL = 1;
                 Engine.LIMIT_TIME = 100;
                 break;
+        }
+    }
+
+    /**
+     * 获取蓝牙适配器，并检测是否支持蓝牙
+     * @return 蓝牙可用则返回true，否则false
+     */
+    private boolean isSupportBluetooth() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null){
+            Toast.makeText(GameActivity.this, R.string.bluetooth_disable, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!gameView.AI){   //非人机模式，即蓝牙模式
+            if(!mBluetoothAdapter.isEnabled()){ //蓝牙未打开
+                //请求打开蓝牙
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
+            }else if(mBluetoothService == null){
+                mBluetoothService = new BluetoothService(this, mHandler);
+                mOutStringBuffer = new StringBuffer("");
+            }
+            startDeviceList();
+        }
+    }
+
+    /**
+     * 打开搜索设别Activity
+     */
+    private void startDeviceList() {
+        Intent serverIntent = new Intent(this, DeviceListActivity.class);
+        startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!gameView.AI){
+            //如果蓝牙服务已停止，则启动服务
+            if(mBluetoothService != null && 
+                mBluetoothService.getState() == BluetoothService.STATE_NONE){
+                LogUtil.d(TAG, "start BT Service");
+                mBluetoothService.start();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(!gameView.AI){
+            if(mBluetoothService != null){  
+                mBluetoothService.stop();   //停止蓝牙服务
+            }
         }
     }
 
@@ -116,7 +202,7 @@ public class GameActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_new_game:
+            case R.id.btn_new_game:     //新游戏
                 LogUtil.i(TAG, "newGameClick");
                 gameView.newGame();
                 if(level == BLUETOOTH){
@@ -128,11 +214,11 @@ public class GameActivity extends Activity implements View.OnClickListener {
                 }
                 startActivityForResult(intentSetting, REQUEST_SETTING);
                 break;
-            case R.id.btn_flip_board:
+            case R.id.btn_flip_board:   //翻转
                 LogUtil.i(TAG,"flipBoardClick");
                 gameView.flipBoard();
                 break;
-            case R.id.btn_undo:
+            case R.id.btn_undo:         //悔棋
                 LogUtil.i(TAG,"undoClick");
                 gameView.undo();
             default:
@@ -153,7 +239,102 @@ public class GameActivity extends Activity implements View.OnClickListener {
                 int side = data.getIntExtra(Setting.MY_SIDE, Setting.OFFENSIVE);
                 int level = data.getIntExtra(Setting.AI_LEVEL, Setting.LOW_RANK);
                 LogUtil.d(TAG, "" + color + side + level);
+                break;
+
+            case REQUEST_CONNECT_DEVICE:
+                connectDevice(data);
+                break;
+
+            case REQUEST_ENABLE_BLUETOOTH:
+                mBluetoothService = new BluetoothService(this, mHandler);
+                mOutStringBuffer = new StringBuffer("");
+                break;
+        }
+    }
+
+    private void connectDevice(Intent data) {
+        // Get the device MAC address
+        String address = data.getExtras()
+                .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        mBluetoothService.connect(device, false);
+    }
+
+    private void sendMessageByBT(String message) {
+        // 发送消息前先确认蓝牙已连接
+        if (mBluetoothService.getState() != BluetoothService.STATE_CONNECTED) {
+            Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT).show();
+            return;
         }
 
+        //切确有消息传送
+        if (message.length() > 0) {
+            byte[] send = message.getBytes();
+            mBluetoothService.write(send);
+
+            mOutStringBuffer.setLength(0);
+
+//            mOutEditText.setText(mOutStringBuffer);
+//            mEditSend.setText(mOutStringBuffer);
+        }
     }
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BluetoothService.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothService.STATE_CONNECTED:
+                            LogUtil.d(TAG,"STATE_CONNECTED");
+                            sendMessageByBT(Build.MODEL);
+//                            setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
+                            break;
+                        case BluetoothService.STATE_CONNECTING:
+                            LogUtil.d(TAG,"STATE_CONNECTING");
+//                            setStatus(R.string.title_connecting);
+                            break;
+                        case BluetoothService.STATE_LISTEN:
+                        case BluetoothService.STATE_NONE:
+                            LogUtil.d(TAG,"STATE_NONE");
+//                            setStatus(R.string.title_not_connected);
+                            break;
+                    }
+                    break;
+                case BluetoothService.MESSAGE_WRITE:
+                    LogUtil.d(TAG,"MESSAGE_WRITE");
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    LogUtil.d(TAG, "send: " + writeMessage);
+//                    mTxtReceive.setText(mTxtReceive.getText().toString() + "\nMe: " + writeMessage);
+//                    mConversationArrayAdapter.add("Me:  " + writeMessage);
+                    break;
+                case BluetoothService.MESSAGE_READ:
+                    LogUtil.d(TAG,"MESSAGE_READ");
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    LogUtil.d(TAG, "receive: " + readMessage);
+//                    mTxtReceive.setText(mTxtReceive.getText().toString() + "\n" + mConnectedDeviceName + ": " + readMessage);
+//                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    break;
+                case BluetoothService.MESSAGE_DEVICE_NAME:
+                    LogUtil.d(TAG,"MESSAGE_DEVICE_NAME");
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(BluetoothService.DEVICE_NAME);
+                    Toast.makeText(getApplication(), "Connected to "
+                            + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    mTxtOppositeName.setText(getString(R.string.opponent) + mConnectedDeviceName);
+                    break;
+                case BluetoothService.MESSAGE_TOAST:
+                    LogUtil.d(TAG,"MESSAGE_TOAST");
+                    Toast.makeText(getApplication(), msg.getData().getString(BluetoothService.TOAST),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 }
